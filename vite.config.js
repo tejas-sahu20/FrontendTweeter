44ec134fa8e93958d0431a4792b4bdf05bed1a40
tejas-sahu20 <105774310+tejas-sahu20@users.noreply.github.com>
2024-07-21 10:40:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './src/',
+  base: '/FrontendTweeter/',
   build: {
     rollupOptions: {
       external: ['@emotion/react']
