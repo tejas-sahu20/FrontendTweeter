66c41a9ae2693af4de30f8f04dfc45c2dbb75252
Tejas Sahu <tejas.sahuprg@gmail.com>
2024-07-21 13:19:20 +0530

    files

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base:"",
-  build: {
-    rollupOptions: {
-      external: ['@emotion/react']
-    }
-  }
 });
